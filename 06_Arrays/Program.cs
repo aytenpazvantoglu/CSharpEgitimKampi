36bd7ed14c04a11d18af4579ee128300aa9e6dde
ayten <ayten.pazvantoglu@gmail.com>
2026-08-12 15:51:22 +0300

    Dizi metotları, kullanıcı girişleri ve sayı sınıflandırma algoritmaları eklendi.
    Diziler bölümü tamamlandı. Konsoldan kullanıcı verisi alma, tam sayı dizilerinde toplam değer hesaplama ve elemanları tek/çift olarak filtreleme pratikleri yapıldı. Yarınki hedef: Foreach döngüleri.

--- 06_Arrays/Program.cs
+++ 06_Arrays/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32.SafeHandles;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -39,6 +40,8 @@ namespace _06_Arrays
             Console.WriteLine(colors[2]);
             // Bu satır dizinin '2' indeksli elemanını yani "Beyaz" yazmamızı sağlar.
 
+            Console.WriteLine();
+            Console.WriteLine("--------------");
 
             //--------------------------------------------------------
             string[] cities = new string[5];
@@ -52,6 +55,9 @@ namespace _06_Arrays
             // Console.WriteLine(cities[5]); -> Bu satırı yazdığımızda hata alırız çünkü 5 elemanlı bir dizide en fazla 4 indeksli eleman vardır.
 
             //--------------------------------------------------------
+
+            Console.WriteLine();
+            Console.WriteLine("--------------");
 
             int[] numbers = new int[10];
 
@@ -90,9 +96,11 @@ namespace _06_Arrays
 
             Console.WriteLine(cities2[2]);
 
-
+            Console.WriteLine();
+            Console.WriteLine("--------------");
 
             #endregion
+
 
             #region Dizideki Tüm Elemanları Listeleme
 
@@ -105,6 +113,8 @@ namespace _06_Arrays
             // Bu kodumuzda oluşturduğumuz dizinin uzunluğu boyunca dizide döngü kullanarak
             // tüm elemanları gezen ve onları ekrana yazdıran bir yapı oluşturduk.
 
+            Console.WriteLine();
+            Console.WriteLine("--------------");
 
             int[] numbers2 = { 4, 85, 96, 75, 125, 635, 488, 522, 7456, 2365, 1120 };
             
@@ -120,9 +130,45 @@ namespace _06_Arrays
                 }
             }
 
+            Console.WriteLine();
+            Console.WriteLine("--------------");
+
             // Bu kodumuzda, oluşturduğumuz dizinin uzunluğu boyunca dizide döngü kullanarak
             // tüm elemanları gezen ve bu elemanlardan 3 sayısına tam bölünenleri 
             // ekrana yazdıran bir yapı oluşturduk.
+
+
+            // Dizilerde char değişkenleri de tanımlayabiliriz.
+            // Ancak char değişkenlerimizi tanımlarken tek tırnak kullanmamız gerekir.
+
+            char[] symbols = { 'a', 'b', 'c', '*', '/', '-' };
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                Console.WriteLine(symbols[i]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("--------------");
+
+            // Dizideki en büyük elemanı bulma
+            int[] myArray = { 47, 85, 95, 41, 25, 635, 789, 86, 100 };
+
+            int maxNumber = myArray[0];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] > maxNumber)
+                {
+                    maxNumber = myArray[i];
+                }
+            }
+            Console.WriteLine(maxNumber);
+
+            Console.WriteLine();
+            Console.WriteLine("--------------");
+
+            #endregion
+
 
             #region UYGULAMA: Dizi içinde Filtreleme ve İstatistik
 
@@ -137,7 +183,7 @@ namespace _06_Arrays
             Console.WriteLine("------------------------------\n");
 
             // 2. Adım: Filtreleme Döngüsü
-            for(int i = 0; i < numbers3.Length; i++)
+            for (int i = 0; i < numbers3.Length; i++)
             {
                 if (numbers3[i] % 3 == 0)
                 {
@@ -203,11 +249,158 @@ namespace _06_Arrays
             #endregion
 
 
+            #region Dizi Metotları
+
+            // Length Metodu -> Dizinin eleman sayısını bulmamıza yarar.
+            string[] persons = { "ali", "ahmet", "ayşe", "buse", "cem", "deniz" };
+            Console.WriteLine(persons.Length);
+
+            Console.WriteLine();
+            Console.WriteLine("--------------");
+
+            // Short Metodu -> Dizinin elemanları küçükten büyüğe doğru sıralamaya yarar.
+            int[] numbers4 = { 45, 85, 52, 41, 86, 10, 22, 35 };
+            Array.Sort(numbers4);
+            for(int i = 0; i < numbers4.Length; i++)
+            {
+                Console.WriteLine(numbers4[i]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("--------------");
+
+            // Reverse Metodu -> Dizinin elemanlarını tam tersi şekilde sıralamaya yarar.
+            int[] numbers5 = { 45, 85, 52, 41, 86, 10, 22, 35 };
+            Array.Reverse(numbers5);
+            for (int i = 0; i < numbers5.Length; i++)
+            {
+                Console.WriteLine(numbers5[i]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("--------------");
+
+            // Short Metodu + Reverse Metodu birlikte kullanılarak -> Dizinin elemanları büyükten küçüğe doğru sıralamaya yarar.
+            int[] numbers6 = { 45, 85, 52, 41, 86, 10, 22, 35 };
+            Array.Sort(numbers6);
+            Array.Reverse(numbers6);
+            for (int i = 0; i < numbers6.Length; i++)
+            {
+                Console.WriteLine(numbers6[i]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("--------------");
+
+            // Max Metodu -> Dizinin en büyük elemanını bulmaya yarar 
+            int[] numbers7 = { 45, 85, 52, 41, 86, 10, 22, 35 };
+            Console.WriteLine(numbers7.Max());
+
+            Console.WriteLine();
+            Console.WriteLine("--------------");
+
+            // Min Metodu -> Dizinin en küçük elemanını bulmaya yarar 
+            int[] numbers8 = { 45, 85, 52, 41, 86, 10, 22, 35 };
+            Console.WriteLine(numbers8.Min());
+
+            Console.WriteLine();
+            Console.WriteLine("--------------");
+
+            Console.WriteLine("-----");
+            int[] numbers9 = { 45, 85, 52, 41, 86, 10, 22, 35 };
+            Console.WriteLine("Dizinin en büyük elemanı: " + numbers9.Max() + " | " + "Dizinin en küçük elemanı: " + numbers9.Min());
+            Console.WriteLine("-----");
+
+            Console.WriteLine();
+            Console.WriteLine("--------------");
+
+            // IndeksOf  Metodu -> Dizi içindeki aranan bir değerin indeks sırasını döndürür.
+            string[] customers = { "ali", "buse", "ayşegül", "merve", "çınar", "kaya" };
+            int index = Array.IndexOf(customers, "merve");
+            Console.WriteLine(index);
+
+
+
+
+
+
             #endregion
 
+            Console.WriteLine();
+            Console.WriteLine("--------------");
+
+            #region Kullanıcıdan Değer Alma
+
+            string[] cities3 = new string[5];
+            
+            for(int i =0; i < cities.Length; i++)
+            {
+                Console.Write($"Lütfen {i + 1}. Şehri Giriniz: ");
+                cities3[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("---------");
+
+            for(int i = 0; i < cities3.Length; i++)
+            {
+                Console.WriteLine(cities3[i]);
+            }
 
 
 
+            #endregion
+           
+            Console.WriteLine();
+            Console.WriteLine("--------------");
+
+            #region Bir Tam Sayı Dizisinde Dizideki Tüm Elamanları Toplama
+            int[] numbers10 = { 10, 20, 30, 40, 50 };
+            int sum = 0;
+
+            for(int i = 0; i < numbers10.Length; i++)
+            {
+                sum += numbers10[i]; 
+            }
+
+            Console.WriteLine(sum);
+
+            #endregion
+
+            Console.WriteLine();
+            Console.WriteLine("--------------");
+
+            #region Dizideki Elemanları Tek veya Çift Olarak Sınıflandırma
+
+            int[] numbers11 = { 21, 42, 33, 54, 55, 66, 897, 748, 39, 220};
+            
+            Console.WriteLine("Çift Sayılar: ");
+            Console.WriteLine();
+
+            for(int i = 0; i < numbers11.Length; i++)
+            {
+                if (numbers11[i] % 2 == 0)
+                {
+                    Console.WriteLine(numbers11[i]);
+                }
+            }
+
+            Console.WriteLine("--------------");
+
+            Console.WriteLine("Tek Sayılar: ");
+            Console.WriteLine();
+
+
+            for (int i = 0; i < numbers11.Length; i++)
+            {
+                if (numbers11[i] % 2 == 1)
+                {
+                    Console.WriteLine(numbers11[i]);
+                }
+            }
+
+
+            #endregion
 
         }
     }
