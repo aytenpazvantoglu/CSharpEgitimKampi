4eb4bf3a9ead1c121e4c1ca747249f9abb8e4b81
Ayten Pazvantoğlu <ayten.pazvantoglu@gmail.com>
2026-08-18 12:05:31 +0300

    Metotlar konusu tamamlandı.

--- 08_Methods/Program.cs
+++ 08_Methods/Program.cs
@@ -20,7 +20,15 @@ namespace _08_Methods
             // Belli işlemleri tekrardan kurtarmak için de programlama için önemli bir yere sahiptir.
 
             // 2- Geriye Değer Döndüren Metotlar
+            // Void yerine döneceği verinin türü (int,string,bool vb.) yazılır.
+            // İşlem sonucunu dışarıya aktarmak için mutlaka "return" anahtar kelimesi kullanılır.
+            // Çağrıldıkları yerde birer veri/değer gibi davranırlar,
+            // bu yüzden gelen sonuç bir değişkene atanabilir veya ekrana yazdırılabilir.
+            // Metodun en başına yazdığımız tür (int, string, bool vb.) neyse,
+            // "return" kelimesinin yanına yazdığımız değerin türü KESİNLİKLE aynı olmalıdır.
 
+
+            #endregion
 
             #region Void Metotlar
 
@@ -49,10 +57,6 @@ namespace _08_Methods
             //Sum();
 
 
-
-
-            #endregion
-
             #region Geriye Değer Döndürmeyen String Parametreli Metotlar
 
             //void WriteMethod(string customerName)
@@ -79,19 +83,114 @@ namespace _08_Methods
 
             #endregion
 
-
             #region Geriye Değer Döndürmeyen Int Parametreli Metotlar
 
-            void Sum(int number1, int number2, int number3)
-            {
-                int result = number1 + number2 + number3;
-                Console.WriteLine(result);
-            }
+            //void Sum(int number1, int number2, int number3)
+            //{
+            //    int result = number1 + number2 + number3;
+            //    Console.WriteLine(result);
+            //}
 
-            Sum(4, 5, 6);
+            //Sum(4, 5, 6);
 
 
             #endregion
+
+
+
+
+
+            #endregion
+
+            #region Geriye Değer Döndüren Metotlar
+
+            //string CustomerName()
+            //{
+            //    return "Buse Yıldız";
+            //}
+
+            //CustomerName();
+
+
+            //string StudentCard()
+            //{
+            //    string name = "Ali";
+            //    string surname = "Kaya";
+
+            //    return name + " " + surname;          
+            //}
+            //Console.WriteLine(StudentCard());
+
+
+            #region Geriye Değer Döndüren String Parametreli Metotlar
+
+            //string CountryCard(string countryName, string capital, string flagColor)
+            //{
+            //    string cardInfo = "Ülke: " + countryName + " - Başkent: " + capital + " - Bayrak Rengi: " + flagColor;
+            //    return cardInfo;
+            //}
+
+            //// Kullanıcıdan veri alarak bunu oluşturabiliriz.
+            //string x, y, z;
+            //Console.Write("Ülke Adını Giriniz: ");
+            //x = Console.ReadLine();
+
+            //Console.Write("Başkenti Giriniz: ");
+            //y = Console.ReadLine();
+
+            //Console.Write("Bayrak Rengini Giriniz: ");
+            //z = Console.ReadLine();
+
+            //Console.WriteLine(CountryCard(x, y, z));
+
+            //// Kendimiz de veri yazarak oluşturabiliriz.
+            //Console.WriteLine(CountryCard("Türkiye", "Ankara", "Kırmızı- Beyaz"));
+
+
+            #endregion
+
+
+            #region Geriye Değer Döndüren Int Parametreli Metotlar
+
+            //int Sum(int number1, int number2)
+            //{
+            //    int result = number1 + number2;
+            //    return result;
+            //}
+            //Console.WriteLine(Sum(45, 98));
+            //Console.WriteLine(Sum(36, 25));
+            //Console.WriteLine(Sum(44, 36));
+            //Console.WriteLine(Sum(14, 29));
+
+
+
+
+
+            #endregion
+
+
+
+            #endregion
+
+            #region Örnek Uygulama 
+
+            string ExamResult(string student, int exam1, int exam2, int exam3)
+            {
+                int result = (exam1 + exam2 + exam3) / 3;
+                if (result >= 50)
+                {
+                    return student + " isimli öğrenci sınavı geçti." + " Ortalaması: " + result;
+                }
+
+                else
+                {
+                    return student + " isimli öğrenci sınavdan geçemedi." + " Ortalaması: " + result;
+                }
+            }
+
+            Console.WriteLine(ExamResult("Ali", 25, 41, 55));
+            Console.WriteLine(ExamResult("Ayşe", 36, 88, 33));
+
 
             #endregion
 
